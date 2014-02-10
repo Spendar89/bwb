2cdbb21dbfe5eeb7dc03ac36ff43a03ff43a60df
Jake Sendar <jakesendar@gmail.com>
2014-02-09 23:06:52 -0500

    adds brand images to homepage

--- app/assets/javascripts/ng/controllers/HomeController.js
+++ app/assets/javascripts/ng/controllers/HomeController.js
@@ -1,28 +1,32 @@
-angular.module('app').controller('HomeCtrl', ['$scope', '$http', '$routeParams', '$location','CurrentEvent', 'Bike', function($scope, $http, $routeParams, $location, CurrentEvent, Bike){
+angular.module('app').controller('HomeCtrl', ['$scope', '$http', '$routeParams', '$location', 'CurrentEvent', 'Bike',
+  function($scope, $http, $routeParams, $location, CurrentEvent, Bike) {
 
 
-  CurrentEvent.query().then(function(data) {
-    $scope.currentEvents = data;
-    $scope.currentEventIndex = 0;
-  });
+    CurrentEvent.query().then(function(data) {
+      $scope.currentEvents = data.slice(0, 3);
+      $scope.currentEventIndex = 0;
+    });
 
-  $scope.$watch("currentEventIndex", function() {
-    if(!$scope.currentEvents) return false;
-      if($scope.currentEventIndex > $scope.currentEvents.length - 1) {
+    // $scope.$watch("currentEventIndex", function() {
+    //   if (!$scope.currentEvents) return false;
+    //   if ($scope.currentEventIndex > $scope.currentEvents.length - 1) {
 
-        $scope.currentEventIndex = 0;
-      }else if($scope.currentEventIndex < 0 ){
-        $scope.currentEventIndex = $scope.currentEvents.length - 1;
-      }else {
-        $scope.currentEvent = $scope.currentEvents[$scope.currentEventIndex];
-      }
+    //     $scope.currentEventIndex = 0;
+    //   } else if ($scope.currentEventIndex < 0) {
+    //     $scope.currentEventIndex = $scope.currentEvents.length - 1;
+    //   } else {
+    //     $scope.currentEvent = $scope.currentEvents[$scope.currentEventIndex];
+    //   }
 
-  });
+    // });
 
-  Bike.smartQuery(function(bikes) {
-    $scope.featuredBikes = bikes.slice(2, 8);
-  });
+    Bike.smartQuery(function(bikes) {
+      $scope.featuredBikes = bikes.slice(2, 8);
+    });
+
+    $scope.brands = ['bianchi', 'blue_cycles', 'breezer', 'electra', 'fuji', 'gt_bicycles', 'khs', 'kona', 'linus', 'marin', 'schwinn', 'scott'];
 
 
 
-}]);
+  }
+]);
